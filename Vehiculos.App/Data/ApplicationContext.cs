d03f2b29c89cea2b2ee7420b61aaa97254551c3c
martin-bianco <martin.bianco@medavente.com>
2022-01-20 22:37:30 -0300

    Add project files.

---
+++ Vehiculos.App/Data/ApplicationContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Vehiculos.App.Models;
+
+namespace Vehiculos.App.Data
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Vehiculo> Vehiculos { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Vehiculo>().ToTable("Vehiculo");
+        }
+    }
+}
---
+++ Vehiculos.App/Models/Vehiculo.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Vehiculos.App.Models
+{
+    public class Vehiculo
+    {
+        [StringLength(maximumLength: 8, MinimumLength = 8, ErrorMessage = "La patente requiere 8 caracteres")]
+        [Required]
+        public string Patente { get; set; }
+
+        [Required]
+        public string Marca { get; set; }
+
+        [Required]
+        public string Modelo { get; set; }
+
+        [Required]
+        public int Puertas { get; set; }
+
+        [Required]
+        public string Titular { get; set; }
+
+    }
+}
